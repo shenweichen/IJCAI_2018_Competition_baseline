ef8b1cb6cd97b6ae87014e70dd0042f7656d05b1
沈伟臣 <last.fantasy@qq.com>
2018-03-19 00:17:12 +0800

    init

---
+++ IJCAI2018_baseline.py
@@ -0,0 +1,66 @@
+import time
+import pandas as pd
+import lightgbm as lgb
+from sklearn.metrics import log_loss
+
+
+def timestamp_datetime(value):
+    format = '%Y-%m-%d %H:%M:%S'
+    value = time.localtime(value)
+    dt = time.strftime(format, value)
+    return dt
+
+
+def convert_data(data):
+    data['time'] = data.context_timestamp.apply(timestamp_datetime)
+    data['day'] = data.time.apply(lambda x: int(x[8:10]))
+    data['hour'] = data.time.apply(lambda x: int(x[11:13]))
+    user_query_day = data.groupby(['user_id', 'day']).size(
+    ).reset_index().rename(columns={0: 'user_query_day'})
+    data = pd.merge(data, user_query_day, 'left', on=['user_id', 'day'])
+    user_query_day_hour = data.groupby(['user_id', 'day', 'hour']).size().reset_index().rename(
+        columns={0: 'user_query_day_hour'})
+    data = pd.merge(data, user_query_day_hour, 'left',
+                    on=['user_id', 'day', 'hour'])
+
+    return data
+
+
+if __name__ == "__main__":
+    online = True
+
+    data = pd.read_csv('round1_ijcai_18_train_20180301.txt', sep=' ')
+    data.drop_duplicates(inplace=True)
+    data = convert_data(data)
+
+    if online == False:
+        train = data.loc[data.day < 24]  # 18,19,20,21,22,23,24
+        test = data.loc[data.day == 24]  # 暂时先使用第24天作为验证集
+    elif online == True:
+        train = data.copy()
+        test = pd.read_csv('round1_ijcai_18_test_a_20180301.txt', sep=' ')
+        test = convert_data(test)
+
+    features = ['item_id', 'item_brand_id', 'item_city_id', 'item_price_level', 'item_sales_level',
+                'item_collected_level', 'item_pv_level', 'user_gender_id', 'user_occupation_id',
+                'user_age_level', 'user_star_level', 'user_query_day', 'user_query_day_hour',
+                'context_page_id', 'hour', 'shop_id', 'shop_review_num_level', 'shop_star_level',
+                'shop_review_positive_rate', 'shop_score_service', 'shop_score_delivery', 'shop_score_description',
+                ]
+    target = ['is_trade']
+
+    if online == False:
+        clf = lgb.LGBMClassifier(
+            'goss', num_leaves=63, max_depth=7, n_estimators=80, n_jobs=20)
+        clf.fit(train[features], train[target], feature_name=features,
+                categorical_feature=['user_gender_id', ])
+        test['lgb_predict'] = clf.predict_proba(test[features],)[:, 1]
+        print(log_loss(test[target], test['lgb_predict']))
+    else:
+        clf = lgb.LGBMClassifier(
+            'goss', num_leaves=63, max_depth=7, n_estimators=80, n_jobs=20)
+        clf.fit(train[features], train[target],
+                categorical_feature=['user_gender_id', ])
+        test['predicted_score'] = clf.predict_proba(test[features])[:, 1]
+        test[['instance_id', 'predicted_score']].to_csv(
+            'baseline.csv', index=False)
